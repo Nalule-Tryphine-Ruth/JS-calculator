efd55cefd16bd6f026ba7279269a839b47f86ae5
Nalule-Tryphine-Ruth <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2022-10-10 19:11:45 +0300

    added comments describing

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -15,14 +15,19 @@ operator =prompt('Enter the operator(+,-,/,*)');
 
 //Display result to the user
 if(operator=='+'){
-result= Number(firstNumber) +Number(secondNumber);
+    //this is the function for adding two numbers
+    result= Number(firstNumber) +Number(secondNumber);
 }else if(operator=='-'){
+    //this is the function for subtracting two numbers
     result= Number(firstNumber) - Number(secondNumber);
-    }else if(operator=='*'){
-        result= Number(firstNumber)*Number(secondNumber);
-    }else if(operator=='/') {
-        result= Number(firstNumber) /Number(secondNumber);
-    } else result ="Error please enter valid operator"
+}else if(operator=='*'){
+    //this is the function for multiplying two numbers
+    result= Number(firstNumber)*Number(secondNumber);
+}else if(operator=='/') {
+    //this is the function for dividing two numbers
+    result= Number(firstNumber) /Number(secondNumber);
+} else result ="Error please enter valid operator"
+    //this is the function for errors
 
 alert(result);
 
